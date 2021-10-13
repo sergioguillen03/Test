8ada08fbd5733a4d393c673676b20eb182430738
sergioguillen03 <84127494+sergioguillen03@users.noreply.github.com>
2021-10-13 13:44:35 -0700

    Add files via upload

---
+++ ClassA.java
@@ -0,0 +1,11 @@
+package Composite;
+
+public class ClassA {
+	String name = "name";
+
+	 public void setName(String name){ this.name = name; }
+
+	 public String getName() {return name; }
+
+	}
+
---
+++ ClassB.java
@@ -0,0 +1,19 @@
+package Composite;
+
+public class ClassB{
+
+	 ClassA classA = new ClassA();
+
+	 
+
+	 public static void main(String[] args) {
+
+	   ClassB b = new ClassB();
+
+	   b.classA.setName("Taylor");
+
+	   System.out.println(b.classA.getName());
+
+	 }
+
+	}
